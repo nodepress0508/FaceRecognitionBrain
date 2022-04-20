e2cd79362e87d107f74a1d9645da6acb60dc5927
nodepress0508 <57334725+nodepress0508@users.noreply.github.com>
2022-04-20 15:25:50 -0700

    Add files via upload
    update

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -19,7 +19,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://guarded-waters-59875.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
